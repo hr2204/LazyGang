46dd18c0034a891781a68d90dba10fdff433ab95
Ren He <rhe@Rens-MacBook-Pro-2.local>
2016-04-07 17:39:43 -0700

    update

--- hitsGenerator/forProblemID.py
+++ hitsGenerator/forProblemID.py
@@ -10,58 +10,70 @@ problemFilePath = "/Users/rhe/Desktop/forId/skill_problem.json"
 jsonObjs = jsonLoader.readJson(problemFilePath)
 # d = json.loads(jsonObjs)
 
-# print type(jsonObjs)
+print type(jsonObjs)
+with open(problemFilePath, 'r') as f:
+    data = f.read()
+
+print data
 
 
+regex_hintId = "\"hintIds\": [.*]"
 
+re.rep
 
 #------------------------------------------------------------
 filePath = "/Users/rhe/Desktop/Grade 7_ Skill 12.4.txt"
-data = ""
-
-with open(filePath, 'r') as f:
-    data= f.read()
-
-
-regex_ae = "Assisted Exercise #\d+\)[\s\S].*?\(Assisted Exercise #\d* - Solution\)"
-
-aeMatches = re.findall(regex_ae, data, re.S)
-
-print "Get {0} AE".format(len(aeMatches))
-aeHints = []
-aeHintsLen = []
-for ae in aeMatches:
-    regex_exe_hints = "\(Assisted Exercise #\d+ - Hints\).*?Tool Tip:"
-    aeHintMatches = re.findall(regex_exe_hints, ae, re.S)
-    aeHintsLen.append(len(aeHintMatches))
+def getIDList( path ):
+    data = ""
+    with open(path, 'r') as f:
+        data= f.read()
 
 
+    regex_ae = "Assisted Exercise #\d+\)[\s\S].*?\(Assisted Exercise #\d* - Solution\)"
 
-regex_exe = "Exercise #\d+\)[\s\S].*?\(Exercise #\d* - Solution\)"
-exeMatches = re.findall(regex_exe, data, re.S)
+    aeMatches = re.findall(regex_ae, data, re.S)
 
-print "Get {0} Exe".format(len(exeMatches))
+    print "Get {0} AE".format(len(aeMatches))
+    aeHints = []
+    aeHintsLen = []
+    for ae in aeMatches:
+        regex_exe_hints = "\(Assisted Exercise #\d+ - Hints\).*?Tool Tip:"
+        aeHintMatches = re.findall(regex_exe_hints, ae, re.S)
+        aeHintsLen.append(len(aeHintMatches))
 
-exeHints = []
-exeHintsLen = []
-for exe in exeMatches:
-    regex_exe_hints = "\(Exercise #\d+ - Hints\).*?Tool Tip:"
-    exeHintMatches = re.findall(regex_exe_hints, exe, re.S)
-    exeHintsLen.append(len(exeHintMatches))
 
-print aeHintsLen
-print exeHintsLen
-totalLen = aeHintsLen + exeHintsLen
-print sum(totalLen)
-hintIds = []
-allIds = range(1,sum(totalLen))
-start = 1
-for i in totalLen:
-    temp = range(start,start+i)
-    start += i
-    hintIds.append(temp)
 
-print hintIds
+    regex_exe = "Exercise #\d+\)[\s\S].*?\(Exercise #\d* - Solution\)"
+    exeMatches = re.findall(regex_exe, data, re.S)
+
+    print "Get {0} Exe".format(len(exeMatches))
+
+    exeHints = []
+    exeHintsLen = []
+    for exe in exeMatches:
+        regex_exe_hints = "\(Exercise #\d+ - Hints\).*?Tool Tip:"
+        exeHintMatches = re.findall(regex_exe_hints, exe, re.S)
+        exeHintsLen.append(len(exeHintMatches))
+
+    print aeHintsLen
+    print exeHintsLen
+    totalLen = aeHintsLen + exeHintsLen
+    print sum(totalLen)
+    hintIds = []
+    allIds = range(1,sum(totalLen))
+    start = 1
+    for i in totalLen:
+        temp = range(start,start+i)
+        start += i
+        hintIds.append(temp)
+
+    return hintIds
+
+hintIds = getIDList(filePath)
+
+
+
+
 
 #-----------output---skill.json
 for i in range(len(jsonObjs)):
---
+++ hitsGenerator/singleColumnHints.py
@@ -0,0 +1,108 @@
+import io
+import re
+import json
+
+
+#filePath = "/Users/hhuang/Desktop/Grade 7- Skill 14.6.txt"
+
+filePath = "/Users/rhe/Downloads/Grade 7_ Skill 13.3.txt"
+data = ""
+
+with open(filePath, 'r') as f:
+    data= f.read()
+
+# regex_ae = "\(Assisted Exercise [#\d]+ - Hints\).*?Tool Tip:"
+
+regex_ae = "\(Assisted Exercise - Hints\).*?Tool Tip:"
+
+matchStrings = re.findall(regex_ae, data, re.S)
+
+print "Get {0} AE hints".format(len(matchStrings))
+
+# for string in matchStrings:
+#    print string
+aeHint = matchStrings[len(matchStrings) - 1]
+
+regex_exe = "Exercise #\d+\)[\s\S].*?\(Exercise #\d* - Solution\)"
+exeMatches = re.findall(regex_exe, data, re.S)
+
+print "Get {0} Exe".format(len(exeMatches))
+
+exeHints = []
+exeHintsLen = []
+for exe in exeMatches:
+    regex_exe_hints = "\(Exercise #\d+ - Hints\).*?Tool Tip:"
+    exeHintMatches = re.findall(regex_exe_hints, exe, re.S)
+    exeHintsLen.append(len(exeHintMatches))
+    exeHints.append(exeHintMatches[len(exeHintMatches)-1])
+
+
+
+#------------get final Exercise Hints---------------------------
+finalExeRight = []
+finalExeLeft = []
+for i in range(0,len(exeHints)): #extHint in exeHints:
+    exHintList = exeHints[i].split("\n")
+    exeAllHints = [x for x in exHintList if x]
+    finalExeRight.append(exeAllHints[-exeHintsLen[i]-1:-1])
+    finalExeLeft.append( exeAllHints[4:4+exeHintsLen[i]])
+# important for Exercise Debug
+for test in finalExeRight:
+    print test
+
+
+# print "Exercise Hint length:"
+# print exeHintsLen
+# print aeHint
+#------------get final AE Hints---------------------------
+aehintList = aeHint.split("\n")
+aeAllHints = [x for x in aehintList if x]
+# print -len(matchStrings)
+finalAE  = aeAllHints[-len(matchStrings)-1:-1]
+finalAELeft = aeAllHints[4:4+len(matchStrings)]
+
+
+# print exeHints
+# print "matchStrings=" + str(len(matchStrings))
+# print aeHint
+
+
+
+
+
+
+
+
+
+# ----------------generate json-------------------------------
+# -------a)-For AE--------------------------------------------
+finalData = []
+dataIndex = 1
+for i in range(0, len(finalAE)):
+        tempObj =\
+        {
+            "hint_id": dataIndex,
+            "type": "mathtex_wrapper",
+            "value": finalAE[i]
+        }
+        finalData.append(tempObj)
+        dataIndex += 1
+print "***final json:***",finalData
+# # -------b)-For Exercise--------------------------------------------
+# # to do for exercise
+#
+for i in range(0, len(finalExeRight)):
+    for j in range(0, len(finalExeRight[i])):
+        tempObj = {
+                "hint_id": dataIndex,
+                "type": "mathtex_wrapper",
+                "value": finalExeRight[i][j]
+            }
+        finalData.append(tempObj)
+        dataIndex += 1
+
+# # ----------------output file-------------------------------
+with open('output.json', 'w') as txtfile:
+    json.dump(finalData, txtfile)
+    # for extHint in exeHints:
+    #     print extHint
