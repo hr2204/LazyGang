571601eee95768cdecdfb5256a7600208b33d010
Benjamin Huang <benjaminhuanghuang@gmail.com>
2016-06-10 11:43:56 -0700

    add readme.md

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# LazyGang
+Problem Generator
+================
 
+Steps
+-----
+
+**Step 1**: Clone the git repository
+
+    $ git clone https://github.com/benjaminhuanghuang/rest_bank_project.git
+    $ cd rest_bank_project
+
+**Step 2**: Create a virtual environment.
+
+For Linux, OSX :
+
+    $ virtualenv venv
+    $ source venv/bin/activate
+    (venv) $ pip install -r requirements.txt
+
+For Windows users working on the standard command prompt:
+
+    > virtualenv venv
+    > venv\scripts\activate
+    (venv) > pip install -r requirements.txt
+
+**Step 3**: Install python library required in project directory
+
+For Linux, OSX :
+    (venv) $ pip install -r requirements.txt
+
+For Windows users working on the standard command prompt:
+    (venv) > pip install -r requirements.txt
+    
+Check library version installed:
+    (venv) $ pip list
--- angular-testing-demo/test/unit/test-app_spec.js
+++ angular-testing-demo/test/unit/test-app_spec.js
@@ -11,6 +11,45 @@ describe('testContrller Test Spec', function() {
 
     describe('Action Handlers ', function() { //describe your app name
 
+        describe("check correct answer", function() {
+            it("Should get correct full name", function() {
+                var $scope = {};
+                var controller = $controller('testController', { $scope: $scope });
+                $scope.firstName = getCorrectAnswerFromAnswerGenerator();
+                $scope.lastName = getCorrectAnswerFromAnswerGenerator();
+
+                $scope.getFullName();
+
+                expect($scope.fullName).toEqual(getCorrectResultFromAnswerGenerator());
+            });
+        });
+
+         describe("check worong answer", function() {
+            it("Should get correct full name", function() {
+                var $scope = {};
+                var controller = $controller('testController', { $scope: $scope });
+                $scope.firstName = getCorrectAnswerFromAnswerGenerator()+"a";
+                $scope.lastName = getCorrectAnswerFromAnswerGenerator()+"b";
+
+                $scope.getFullName();
+
+                expect($scope.fullName).toNotEqual(getCorrectResultFromAnswerGenerator());
+            });
+        });
+
+        describe("check incomplete answer", function() {
+            it("Should get correct full name", function() {
+                var $scope = {};
+                var controller = $controller('testController', { $scope: $scope });
+                $scope.firstName = "";
+                $scope.lastName = null;
+
+                $scope.getFullName();
+
+                expect($scope.fullName).toNotEqual(-1);
+            });
+        });
+
         describe("getFullName", function() {
             it("Should get correct full name", function() {
                 var $scope = {};
